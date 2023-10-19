04270d3826d6998a0a2c720ea423d5460295ee98
EdsonNhancale <edsonnhancale97@gmail.com>
2023-10-19 22:58:32 +0200

    feat: added function to get save and get names from databases

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # docker-nginx-node-fullcycle
+
+``` Nodejs server with Nginx as reverse proxy ```
+
+## How to pull from Docker Hub
+
+``` docker pull edsonnhancale/nginx-proxy ```
+
+``` docker pull edsonnhancale/node-nginx```
+
+## How to run
+
+``` docker-compose up -d ```
+
+## How to test
+``` curl http://localhost:8080 ```
--- node/index.js
+++ node/index.js
@@ -1,8 +1,8 @@
 const express = require("express")
-const app =express()
+const app = express()
 const port = 3000
 const config = {
-    host:'db',
+    host: 'db',
     user: 'root',
     password: 'root',
     database: 'nodedb'
@@ -10,12 +10,25 @@ const config = {
 const mysql = require('mysql')
 
 const connection = mysql.createConnection(config)
+let names= null
 
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+    var sql = "CREATE TABLE IF NOT EXISTS people (name VARCHAR(255), id INT AUTO_INCREMENT PRIMARY KEY)";
+    connection.query(sql);
+    connection.query('INSERT INTO people(name) values("Edson")')
+    connection.query('SELECT name FROM people', function (err, result, fields) {
+        if (err) throw err;
+        names =result
+    });
+    connection.end()
+});
 
-app.get('/', (req, res)=>{
-    res.send('<h1>Full Cycle</h1>')
+app.get('/', (req, res) => {
+    res.send(`<h1>Full Cycle Rocks!v2</h1>`+JSON.stringify(names))
 })
 
-app.listen(port,()=>{
-    console.log('Rodando na porta '+ port)
+app.listen(port, () => {
+    console.log('Rodando na porta ' + port)
 })
